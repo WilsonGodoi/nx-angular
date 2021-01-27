79ad04613d984a958fd876f85d64d0256f268ab5
GODOI Wilson Goivinho <wilsongoivinho.godoi.ext@altran.com>
2021-01-27 12:25:23 -0300

    feat(login): do login

--- libs/feature-login/src/lib/feature-login.module.ts
+++ libs/feature-login/src/lib/feature-login.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { FeatureLoginRoutingModule } from './feature-login-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UiPrimengModule } from '@nx-angular/ui-primeng';
 
 @NgModule({
-  imports: [CommonModule, FeatureLoginRoutingModule],
-  declarations: [LoginComponent]
+  imports: [
+    CommonModule,
+    FeatureLoginRoutingModule,
+    ReactiveFormsModule,
+    UiPrimengModule,
+  ],
+  declarations: [LoginComponent],
 })
 export class FeatureLoginModule {}
--- libs/feature-login/src/lib/login/login.component.ts
+++ libs/feature-login/src/lib/login/login.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'nx-angular-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss'],
+  animations: [
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotateY(0)' })),
+      state('rotated', style({ transform: 'rotateY(-180deg)' })),
+      transition('rotated => default', animate('800ms ease-out')),
+      transition('default => rotated', animate('800ms ease-in')),
+    ]),
+  ],
 })
 export class LoginComponent implements OnInit {
+  public state: string;
+  public loading: boolean;
 
-  constructor() { }
+  public userform: FormGroup;
+
+  constructor(private router: Router, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.userform = this.fb.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    this.state = 'default';
   }
 
+  public doLogin(e?: Event) {
+    if (this.userform.valid) {
+      e.preventDefault();
+      this.loading = true;
+
+      of(true)
+        .pipe(delay(1000))
+        .subscribe(
+          (success) => {
+            this.rotate();
+            setTimeout(() => {
+              this.router.navigate(['dashboard']);
+            }, 800);
+          },
+          (error) => {
+            this.loading = false;
+          }
+        );
+    } else {
+      Object.keys(this.userform.controls).map((field) =>
+        this.userform.get(field).markAsDirty()
+      );
+    }
+  }
+
+  private rotate(): void {
+    this.state = this.state === 'default' ? 'rotated' : 'default';
+  }
 }
