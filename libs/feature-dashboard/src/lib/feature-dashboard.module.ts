bfad45cb0a61677e645c70146bd94eac32185a6d
GODOI Wilson Goivinho <wilsongoivinho.godoi.ext@altran.com>
2021-01-28 15:43:07 -0300

    fix(primeng): removed primeng ui from dashboard module

--- libs/feature-dashboard/src/lib/feature-dashboard.module.ts
+++ libs/feature-dashboard/src/lib/feature-dashboard.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FeatureDashboardRoutingModule } from './feature-dashboard-routing.module';
+import { NgModule } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { UiPrimengModule } from '@nx-angular/ui-primeng';
+import { FeatureDashboardRoutingModule } from './feature-dashboard-routing.module';
+
+const PrimengModules = [ChartModule];
 
 @NgModule({
-  imports: [CommonModule, FeatureDashboardRoutingModule, UiPrimengModule],
+  imports: [CommonModule, FeatureDashboardRoutingModule, ...PrimengModules],
   declarations: [DashboardComponent],
 })
 export class FeatureDashboardModule {}
